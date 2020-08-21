7792cc273e588fbe8bce3d45a614fe1c56ac45f9
HsuTing <hsuting0106@gmail.com>
2020-08-22 01:25:49 +0800

    feat(stories): add antd custom theme

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,22 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+  stories: ['../stories/**/*.stories.js'],
+  webpackFinal: config => {
+    config.module.rules.push({
+      test: /\.less$/,
+      use: [
+        require.resolve('style-loader'),
+        require.resolve('css-loader'),
+        {
+          loader: require.resolve('less-loader'),
+          options: {
+            lessOptions: {
+              javascriptEnabled: true,
+            },
+          },
+        },
+      ]
+    });
+
+    return config;
+  }
+};
---
+++ stories/Button.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from 'antd';
+
+import './styles/index.less';
+
+export default {
+  title: 'Components/Button',
+  component: Button,
+}
+
+export const Example = () => <Button type="primary">test</Button>;
